c941106df528e39a7d373a383c55fd3ea4cf08fa
Kaye Ng <ngkvalerie@gmail.com>
2019-01-24 20:37:34 +0800

    fixes

--- wordcounter.py
+++ wordcounter.py
@@ -9,9 +9,9 @@ def main():
 
     for line in lines:
         result = re.findall(r'([\w+’()-]*)', line)
-        for i in result:
-            if len(i.strip()) > 0:
-                items.append(i.strip())
+        for x in result:
+            if len(x.strip('’')) > 0:
+                items.append(x.strip())
 
     items = [re.sub(r'[()]', '', x) if '(' in x else x for x in items]
     items = [re.sub(r'’', '', x) if re.fullmatch(r'(.*?)’', x) is not None else x for x in items]
@@ -19,7 +19,8 @@ def main():
     word_count_dict = Counter(items)
 
     print("Total words: " + str(len(word_count_dict)))
-    print(word_count_dict)
+    for key, value in word_count_dict.items():
+        print(key, '-', value)
 
 
 main()
