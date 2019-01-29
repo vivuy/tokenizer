12ca2735ae32438f4bb96682a7d1e0e93c443d32
Vivian Uy <vivian_uy@dlsu.edu.ph>
2019-01-29 23:10:23 +0800

    added test cases

--- Minimum Edit Distance/main.py
+++ Minimum Edit Distance/main.py
@@ -1,31 +1,28 @@
-source = "bugs bunny"
-target = "big chungus"
-
-D = [[0 for j in range(len(target)+1)] for i in range(len(source)+1)]
-
-
-def initialize():
+def initialize(source, target):
+    d = [[0 for a in range(len(target) + 1)] for b in range(len(source) + 1)]
     for i in range(len(source)+1):
         for j in range(len(target)+1):
-            D[i][0] = i
-            D[0][j] = j
+            d[i][0] = i
+            d[0][j] = j
+    compute(source, target, d)
 
 
-def compute():
+def compute(source, target, d):
     for j in range(1, len(target)+1):
         for i in range(1, len(source)+1):
             if source[i-1] == target[j-1]:
-                D[i][j] = D[i-1][j-1]
+                d[i][j] = d[i-1][j-1]
             else:
-                D[i][j] = min([D[i][j-1] + 1, D[i-1][j] + 1, D[i-1][j-1] + 2])
+                d[i][j] = min([d[i][j-1] + 1, d[i-1][j] + 1, d[i-1][j-1] + 2])
+    get_path(source, target, d)
 
 
-def print_table():
-    for i in range(len(D)):
-        print(D[i])
+# def print_table():
+#     for i in range(len(D)):
+#         print(D[i])
 
 
-def get_path():
+def get_path(source, target, d):
     path = ""
     i = len(source)
     j = len(target)
@@ -40,30 +37,34 @@ def get_path():
             i -= 1
             j -= 1
         else:
-            minimum = min(D[i-1][j], D[i][j-1], D[i-1][j-1])
-            if minimum == D[i-1][j]:
+            minimum = min(d[i-1][j], d[i][j-1], d[i-1][j-1])
+            if minimum == d[i-1][j]:
                 path += "D"
                 new_source += source[i-1]
                 new_target += "-"
                 i -= 1
-            elif minimum == D[i][j-1]:
+            elif minimum == d[i][j-1]:
                 path += "I"
                 new_source += "-"
                 new_target += target[j - 1]
                 j -= 1
-            elif minimum == D[i-1][j-1]:
+            elif minimum == d[i-1][j-1]:
                 path += "S"
                 new_source += source[i-1]
                 new_target += target[j - 1]
                 i -= 1
                 j -= 1
-    print("Distance: " + str(D[len(source)][len(target)]))
+    print("Distance: " + str(d[len(source)][len(target)]))
     print("--------")
     print(new_source[::-1])
     print(path[len(target)::-1])
-    print(new_target[::-1])
+    print(new_target[::-1] + '\n')
 
 
-initialize()
-compute()
-get_path()
+initialize("bugs bunny", "big chungus")
+initialize("naruto's son", "boruto's dad")
+initialize("kumakain", "kumain")
+initialize("levinstien", "levenshtein")
+initialize("leviathan", "levenshtein")
+initialize("ATGCATCCCATGAC", "TCTATATCCGT")
+initialize("AGGCTATCACCTGACCTCCAGGCCGATGCCCACCTGG", "TAGCTATCACGACCGCGGTCGATTTGCCCGACGGTCC")
