5eded5a6c55b51bd824440703e64404020a2e9be
Vivian Uy <vivian_uy@dlsu.edu.ph>
2019-01-29 22:59:26 +0800

    with -

--- Minimum Edit Distance/main.py
+++ Minimum Edit Distance/main.py
@@ -1,5 +1,5 @@
-source = "levinstien"
-target = "levenshtein"
+source = "bugs bunny"
+target = "big chungus"
 
 D = [[0 for j in range(len(target)+1)] for i in range(len(source)+1)]
 
@@ -27,30 +27,43 @@ def print_table():
 
 def get_path():
     path = ""
-    i = len(source) #
-    j = len(target) #
+    i = len(source)
+    j = len(target)
+    new_source = ""
+    new_target = ""
 
     while not (i == 0 and j == 0):
         if source[i-1] == target[j-1]:
             path += "M"
+            new_source += source[i-1]
+            new_target += target[j-1]
             i -= 1
             j -= 1
         else:
             minimum = min(D[i-1][j], D[i][j-1], D[i-1][j-1])
             if minimum == D[i-1][j]:
                 path += "D"
+                new_source += source[i-1]
+                new_target += "-"
                 i -= 1
             elif minimum == D[i][j-1]:
                 path += "I"
+                new_source += "-"
+                new_target += target[j - 1]
                 j -= 1
             elif minimum == D[i-1][j-1]:
                 path += "S"
+                new_source += source[i-1]
+                new_target += target[j - 1]
                 i -= 1
                 j -= 1
-    return print(path[len(target)::-1])
+    print("Distance: " + str(D[len(source)][len(target)]))
+    print("--------")
+    print(new_source[::-1])
+    print(path[len(target)::-1])
+    print(new_target[::-1])
 
 
 initialize()
 compute()
 get_path()
-print(D[len(source)][len(target)])
